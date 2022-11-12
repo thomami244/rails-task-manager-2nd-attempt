0ff272c2034def9af9136adc97d4cf11da34d7eb
Michael Thomas <mike.ahpong@gmail.com>
2022-11-12 20:17:27 +0800

    all crud actions completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,7 +17,26 @@ class TasksController < ApplicationController
     redirect_to task_path(task.id)
   end
 
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to task_path(task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+
+  end
+
+  private
   def task_params
     params.require(:task).permit(:title, :details)
   end
+
 end
