55ccdfe66d48be4cf683e88537cc3af09590bcfd
Michael Thomas <mike.ahpong@gmail.com>
2022-11-12 13:30:05 +0800

    completed until update task list

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
